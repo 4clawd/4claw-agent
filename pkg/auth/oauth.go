2c12d7f71b3977aee055ef36d39e88d47904bca8
Your Name <you@example.com>
2026-03-14 04:18:39 +0800

    update the bug

--- pkg/auth/oauth.go
+++ pkg/auth/oauth.go
@@ -643,7 +643,7 @@ func OpenBrowser(url string) error {
 	case "linux":
 		return exec.Command("xdg-open", url).Start()
 	case "windows":
-		return exec.Command("cmd", "/c", "start", "", url).Start()
+		return exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", url).Start()
 	default:
 		return fmt.Errorf("unsupported platform: %s", runtime.GOOS)
 	}
